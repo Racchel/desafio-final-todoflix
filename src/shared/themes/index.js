288e25bed7a969700c31e00e7f569674a0b1dc6c
Racchel <racchelvelasco@gmail.com>
2022-04-14 19:56:56 -0300

    [FRONT] Export theme

--- src/shared/themes/index.js
+++ src/shared/themes/index.js
@@ -1,1 +1,18 @@
-export { }
+import { colors } from './colors'
+import { useBreakpoints } from './breakpoints'
+import { font } from './font'
+
+/**
+ * xs = eXtremely small
+ * sm = small. 
+ * md = medium. 
+ * lg = large. 
+ * xl = eXtremely large
+ * v = variation. 
+ * default = unique
+ */
+export const theme = {
+  colors,
+  useBreakpoints,
+  font
+}

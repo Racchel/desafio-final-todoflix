9920aa638b3369d7f83d5856fa39b408c28a5afe
Racchel <racchelvelasco@gmail.com>
2022-04-14 19:52:14 -0300

    [FRONT] Create a custom color palette

---
+++ src/shared/themes/colors.js
@@ -0,0 +1,18 @@
+export const colors = {
+  neutral: {
+    xs: '#FFFFFF',
+    sm: '#B1B1B1',
+    md: '#717171',
+    lg: '#2C2C2C',
+    xl: '#000000'
+  },
+  primary: {
+    default: '#E71B27'
+  },
+  level: {
+    v1: '#E71B27',
+    v2: '#E08D1B',
+    v3: '#6CBE61'
+  }
+}
+

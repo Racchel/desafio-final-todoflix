9dc66f43f69ed204c33bf8130f2a0ad61c4e8e00
Racchel <racchelvelasco@gmail.com>
2022-04-14 19:53:15 -0300

    [FRONT] Create custom font style

---
+++ src/shared/themes/fonts.js
@@ -0,0 +1,23 @@
+export const font = {
+  family: {
+    default: 'Arial, sans-serif'
+  },
+  size: {
+    xs: '12px',
+    sm: '14px',
+    md: '16px',
+    lg: '30px',
+    xl: '40px'
+  },
+  weight: {
+    sm: '400',
+    lg: '500'
+  },
+  line_height: {
+    xs: '14px',
+    sm: '16px',
+    md: '18px',
+    lg: '32px',
+    xl: '42px'
+  }
+}

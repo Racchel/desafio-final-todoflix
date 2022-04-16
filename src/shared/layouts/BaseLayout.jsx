eccb7af050bbe5ca2d43ca4320f2968db9165d20
Racchel <racchelvelasco@gmail.com>
2022-04-16 14:52:11 -0300

    [FRONT] Use header component in BaseLayout

--- src/shared/layouts/BaseLayout.jsx
+++ src/shared/layouts/BaseLayout.jsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
 
+/** components */
+import { Header } from '../components'
+
 /** theme */
 
+/** styled */
 const Container = styled.div`
   width: 100%;
   min-height: 100vh;
@@ -10,6 +14,7 @@ const Container = styled.div`
 const BaseLayout = ({ title, children }) => {
   return (
     <Container>
+      <Header />
       <h1>{title}</h1>
       {children}
     </Container>
